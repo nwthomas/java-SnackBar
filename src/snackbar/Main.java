f2bc8bc60b1f6abd5c3083c5811f65d682929efd
Nathan Thomas <nwthomas@me.com>
2019-05-27 22:35:27 -0700

    Build out entire Snack.java class in file

--- src/snackbar/Main.java
+++ src/snackbar/Main.java
@@ -1,11 +1,15 @@
 package snackbar;
 
-public class Main {
-  private static void workWithData() {
+public class Main 
+{
+
+  private static void workWithData() 
+  {
     // Finish
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     workWithData();
   }
 }
--- src/snackbar/Snack.java
+++ src/snackbar/Snack.java
@@ -1,1 +1,71 @@
 package snackbar;
+
+public class Snack
+{
+	private static int maxId = 0;
+	private int id;
+	private String name;
+	private int quantity; // Possibly double later
+	private int cost; // Possibly double later
+	private int vendingMachineId; // Possibly double later
+
+	public Snack(String name, int quantity, int cost, int vendingMachineId)
+	{
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this.vendingMachineId = vendingMachineId;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getQuantity()
+	{
+		return quantity;
+	}
+
+	public void setQuantity(int quantity)
+	{
+		this.quantity = quantity;
+	}
+
+	public int getCost()
+	{
+		return cost;
+	}
+
+	public void setCost(int cost)
+	{
+		this.cost = cost;
+	}
+
+	public int getVendingMachineId()
+	{
+		return vendingMachineId;
+	}
+
+	public void setVendingMachineId(int vendingMachineId)
+	{
+		this.vendingMachineId = vendingMachineId;
+	}
+}
