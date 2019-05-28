674491d960ce406652d900fb9ec2df1b3317ab98
Nathan Thomas <nwthomas@me.com>
2019-05-28 00:49:40 -0700

    Add first two processing objectives

--- src/snackbar/Main.java
+++ src/snackbar/Main.java
@@ -21,6 +21,28 @@ public class Main
   	Snack sSoda = new Snack("Soda", 24, 2.50, vDrink.getId());
   	Snack sWater = new Snack("Water", 20, 2.75, vDrink.getId());
 
+  	// Processing via README instructions
+  	System.out.println("1. Customer 1 buys 3 of snack 4. Print Customer 1 Cash on hand. Print quantity of snack 4.");
+  	double purchase1 = 3 * sSoda.getCost();
+  	c1.makePurchase(purchase1);
+  	sSoda.buySnack(3);
+  	System.out.println("Customer 1 cash: " + c1.getCash());
+  	System.out.println("Snack 4 quantity: " + sSoda.getQuantity());
+  	System.out.println();
+
+  	System.out.println("2. Customer 1 buys 1 of snack 3. Print Customer 1 Cash on hand. Print quantity of snack 3.");
+  	double purchase2 = 1 * sPretzel.getCost();
+  	c1.makePurchase(purchase2);
+  	sPretzel.buySnack(1);
+  	System.out.println("Customer 1 cash: " + c1.getCash());
+  	System.out.println("Snack 3 quantity: " + sPretzel.getQuantity());
+  	System.out.println();
+
+  	System.out.println("3. Customer 2 buys 2 of snack 4. Print Customer 2 Cash on Hand. Print quantity of snack 4.")
+
+
+
+  	// Extra Processing
   	System.out.println(c1.toString());
   	System.out.println(c2.toString());
 
